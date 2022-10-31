5a1d5933ffd687ba226a9bd449c680b2df59d629
yimojeff <36731636+yimojeff@users.noreply.github.com>
2022-10-31 20:48:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ def getCommand(prompt: str='请输入命令') -> None:
     undo_command : str = ''#记录上次执行的add、del操作
     redo_command : str = ''#记录上次执行的undo操作
     try:
-        if(command.split()[0].startswith("Add") or command.split()[0].startswith("Del"))
+        if(command.split()[0].startswith("add") or command.split()[0].startswith("del"))
             undo_command = command
         module: Command = commands[command.split()[0]]()
         
     except Exception as e:
         print('命令错误，请重试')
         
-    if(command.split()[0].startswith("Und") and Undo_command != '')
+    if(command.split()[0].startswith("und") and Undo_command != '')
         args.append(Undo_command)
         redo_command = undo_command
         undo_command = ''
         
-    if(command.split()[0].startswith("Red") and Redo_command != '')
+    if(command.split()[0].startswith("red") and Redo_command != '')
         args.append(redo_command)
         Undo_command = Redo_command
         Redo_command = ''

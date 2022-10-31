73d304bd1c30e7f5b22f00531b8d39f35bb793d1
yimojeff <36731636+yimojeff@users.noreply.github.com>
2022-10-31 20:50:02 +0800

    Update UndoCommand.py

--- Command/implement/UndoCommand.py
+++ Command/implement/UndoCommand.py
@@ -9,4 +9,20 @@ class UndoCommand(Command):
         super().__init__()
         
     def exec(self, instance: Bookmark, *args) -> None:
-        return super().exec(*args)
+        if(len(args) != 1):
+            print('There is no Add or Del operation to Undo')
+            return
+        #args: List[str] = command.split()[1:]
+        #command.split()[0]
+        if(command.split()[0] == "add-title")
+            #del-title function
+        if(command.split()[0] == "add-bookmark")
+            #del-bookmark function
+        if(command.split()[0] == "delete-title")
+            #add-title function
+        if(command.split()[0] == "delete-bookmark")
+            #add-bookmark function
+        
+        
+        
+        return 
